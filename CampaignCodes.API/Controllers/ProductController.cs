02ac1c503f068392fbde8a4e0e2225d32c24d673
kmturhan <kmturhan@gmail.com>
2023-06-14 14:32:26 +0300

    feat: code generator added

---
+++ CampaignCodes.API/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿
+using CampaignCodes.Core.Repository.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CampaignCodes.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductController : ControllerBase
+	{
+		Code code = new Code();
+		[HttpGet("GetList")]
+		public List<Code> GetList()
+		{
+			return code.GetCodeList();
+		}
+		// GET: api/<ProductController>
+		[HttpPost("CodeGenerates")]
+		public IEnumerable<Code> CodeGenerates(CodeParameter parameter)
+		{
+			return code.CodeGenerates(parameter.CodeLength, parameter.CodeCount); ;
+		}
+		
+		// POST api/<ProductController>/5
+		[HttpPost("Check")]
+		public Code Check(Code code)
+		{
+			return code.CodeCheck(code.UniqueCode); ;
+		}
+	}
+}
+public class CodeParameter
+{
+	public int CodeLength { get; set; } = 8;
+	public int CodeCount { get; set; } = 1000;
+}
---
+++ CampaignCodes.Core/Repository/Abstract/ICode.cs
@@ -0,0 +1,17 @@
+﻿using CampaignCodes.Core.Repository.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignCodes.Core.Repository.Abstract
+{
+	public interface ICode
+	{
+		List<Code> GetCodeList();
+		List<Code> CodeGenerates(int codeLength, int codeCount);
+		List<Code> CodeCheck(List<string> codes);
+		Code CodeCheck(string code);
+	}
+}
